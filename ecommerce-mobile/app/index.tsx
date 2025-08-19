730fdb936c8ca92a9cff71bd89f7cb66acf8e4d8
Juraj Mudroch <mudroch.juraj@protonmail.com>
2025-08-19 14:35:17 +0200

    initial

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,13 +1,16 @@
-import {FlatList} from "react-native";
-import products from '../assets/products.json'
-import ProductListItem from '../components/ProductListItem';
-
+// import {FlatList} from "react-native";
+// import products from '../assets/products.json'
+// import ProductListItem from '../components/ProductListItem';
+import {Button,ButtonText } from "@/components/ui/button";
 
 export default function HomeScreen() {
     return (
-        <FlatList
-            data={products}
-            renderItem={({item}) => <ProductListItem product={item}/>}
-        />
+        <Button variant="outline" size="xl">
+            <ButtonText>press me</ButtonText>
+        </Button>
+        // <FlatList
+        //     data={products}
+        //     renderItem={({item}) => <ProductListItem product={item}/>}
+        // />
     )
 }
--- ecommerce-mobile/components/ProductListItem.tsx
+++ ecommerce-mobile/components/ProductListItem.tsx
@@ -1,5 +1,6 @@
 import {Text} from "react-native";
 
+// @ts-ignore
 export default function ProductListItem({product}) {
     return <Text style={{fontSize: 30}}>{product.name}</Text>
 }
