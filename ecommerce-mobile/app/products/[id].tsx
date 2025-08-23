dc891900768588537e8911e9b943295ba7e576fb
Juraj Mudroch <mudroch.juraj@protonmail.com>
2025-08-23 12:53:13 +0200

    initial

--- ecommerce-mobile/app/products/[id].tsx
+++ ecommerce-mobile/app/products/[id].tsx
@@ -5,6 +5,9 @@ import {Card} from "@/components/ui/card";
 import {Image} from "@/components/ui/image";
 import {VStack} from "@/components/ui/vstack";
 import {Heading} from "@/components/ui/heading";
+import {Box} from "@/components/ui/box";
+import {Button} from "@/components/ui/button";
+import {ButtonText} from "@/components/ui/button";
 export default function ProductDetails() {
     const { id } = useLocalSearchParams();
 
@@ -31,7 +34,21 @@ export default function ProductDetails() {
                 <Heading size="xl">
                     ${product.price}
                 </Heading>
+                <Text size="sm" className="pb-3 pt-1">{product.description}</Text>
             </VStack>
+            <Box className="flex-col sm:flex-row">
+                <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
+                    <ButtonText size="sm">Add to cart</ButtonText>
+                </Button>
+                <Button
+                    variant="outline"
+                    className="px-4 py-2 border-outline-300 sm:flex-1"
+                >
+                    <ButtonText size="sm" className="text-typography-600">
+                        Wishlist
+                    </ButtonText>
+                </Button>
+            </Box>
         </Card>
 
     )
--- ecommerce-mobile/components/ProductListItem.tsx
+++ ecommerce-mobile/components/ProductListItem.tsx
@@ -1,10 +1,6 @@
 import {Card} from "@/components/ui/card";
 import {Image} from "@/components/ui/image";
-import {Button} from "@/components/ui/button";
 import {Text} from "@/components/ui/text";
-import {Box} from "@/components/ui/box";
-import {ButtonText} from "@/components/ui/button";
-import {VStack} from "@/components/ui/vstack";
 import {Heading} from "@/components/ui/heading";
 import {Link} from "expo-router";
 import {Pressable} from "react-native";
@@ -12,24 +8,24 @@ import {Pressable} from "react-native";
 // @ts-ignore
 export default function ProductListItem({product}) {
     return (
-        <Link href={`/products/${product.id}`} asChild className="flex">
+        <Link href={`/products/${product.id}`} asChild>
             <Pressable className='flex-1'>
-                <Card className="p-5 rounded-lg max-w-[360px] flex space-between">
+                <Card className="p-5 rounded-lg max-w-[360px] h-[390px] justify-end">
                     <Image
                         source={{
                             uri: product.image,
                         }}
-                        className="h-[280px] rounded-md w-full"
+                        className="h-[260px] rounded-md w-full"
                         alt={`${product.name} image`}
                         resizeMode="contain"
                     />
                     <Text
-                        className="text-sm font-normal text-typography-700 h-[60px]"
+                        className="text-sm font-normal text-typography-700 mt-4"
                     >
-                       product.name
+                        {product.name}
 
                     </Text>
-                        <Heading size="md" className="pt-4">
+                        <Heading size="md" className="mt-2">
                             ${product.price}
                         </Heading>
                 </Card>
