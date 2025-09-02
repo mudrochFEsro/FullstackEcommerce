5fac0c2b7d36b108995aa8b7f660bdec1cd45008
Juraj Mudroch <mudroch.juraj@protonmail.com>
2025-09-02 09:07:36 +0200

    initial

--- ecommerce-mobile/app/cart.tsx
+++ ecommerce-mobile/app/cart.tsx
@@ -1,9 +1,27 @@
-import {Text, View} from "react-native";
+import {FlatList} from "react-native";
+import {useCart} from "@/store/cartStore";
+import {Text} from "@/components/ui/text";
+import {HStack} from "@/components/ui/hstack";
+import {VStack} from "@/components/ui/vstack";
 
-export default function CartScreen(){
+export default function CartScreen() {
+    // @ts-ignore
+    const items = useCart(state => state.items);
+
+    console.log(items);
+
     return (
-        <View>
-            <Text style={{fontSize: 30}}>Cart Screen</Text>
-        </View>
+        <FlatList
+            data={items}
+            renderItem={({item}) => (
+                <HStack className='bg-white p-3'>
+                    <VStack space='sm'>
+                        <Text>{item.product.name}</Text>
+                        <Text>{item.product.price}</Text>
+                    </VStack>
+                    <Text className='ml-auto'>{item.quantity}</Text>
+                </HStack>
+            )}
+        />
     )
 }
